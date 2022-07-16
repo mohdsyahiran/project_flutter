3b891e9e1456afa848c73415b23fb95df97f43ff
Syahiran Azizan <79651001+mohdsyahiran@users.noreply.github.com>
2022-07-16 22:54:16 +0800

    Add files via upload

---
+++ login/lib/Screens/Welcome/components/welcome_image.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../constants.dart';
+
+class WelcomeImage extends StatelessWidget {
+  const WelcomeImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "WELCOME TO LIFESAVER : HEALTH GUARDIAN!",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: defaultPadding * 2),
+        Row(
+          children: [
+            Spacer(),
+
+            Spacer(),
+          ],
+        ),
+        SizedBox(height: defaultPadding * 2),
+      ],
+    );
+  }
+}
